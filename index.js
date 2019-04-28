7b18cc94cc014bdc6e33d20313bf725ce4b4b0e4
gagi-palma <vilijemvolas@gmail.com>
2019-04-28 18:52:37 +0200

    pravljenje hedera

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("navLeft").style.width = "40%";
+    document.getElementById("navbar").style.marginLeft = "40%";
+}
+
+function closeNav() {
+    document.getElementById("navLeft").style.width = "0";
+    document.getElementById("navbar").style.marginLeft = "0";
+}
