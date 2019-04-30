dc34da68b8ff7c6f89a67b269ae4f9723ab805c0
gagi-palma <vilijemvolas@gmail.com>
2019-04-30 18:32:21 +0200

    Dodavanje slidera

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    var captionText = document.getElementById("text");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    let x = document.getElementById(slideIndex).alt;
+    captionText.innerHTML = x;
+}
+
+setInterval(function () {
+    slideIndex++;
+    showSlides(slideIndex);
+}, 3000);
+
 function openNav() {
-    document.getElementById("navLeft").style.width = "40%";
-    document.getElementById("navbar").style.marginLeft = "40%";
+    document.getElementById("navLeft").style.width = "40vw";
+    document.getElementById("container").style.marginLeft = "40vw";
 }
 
 function closeNav() {
     document.getElementById("navLeft").style.width = "0";
-    document.getElementById("navbar").style.marginLeft = "0";
+    document.getElementById("container").style.marginLeft = "0";
 }
